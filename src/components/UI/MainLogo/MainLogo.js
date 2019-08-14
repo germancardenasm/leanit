94b8515d9dee2cd1b343031235ba9708fe0f054d
germancardenasm <germancardenas@gmail.com>
2019-08-14 18:02:19 -0500

    Add main file structure

---
+++ src/components/UI/MainLogo/MainLogo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/images/logo.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="Transport Inc Logo" />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/containers/ControlPanel/ControlPanel.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./ControlPanel.css";
+
+export default class Panel extends Component {
+  render() {
+    return <div className="controlPanel">Main panel</div>;
+  }
+}
