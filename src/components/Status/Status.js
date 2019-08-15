878bbb4366324b95763f618f607314aeb2e9db11
germancardenasm <germancardenas@gmail.com>
2019-08-15 11:28:18 -0500

    Add Button Component

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Status.css";
 import Title from "../UI/Title/Title";
+import Button from "../UI/Button/Button";
 
 const Status = () => {
   return (
     <div>
       <Title title="Status" />
+      <Button name="DELIVERED" />
     </div>
   );
 };
